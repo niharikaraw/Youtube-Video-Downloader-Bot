d94ad29271da08c384690e60507c00d89bb2045b
niharikaraw <nrawat.kol@gmail.com>
2022-12-07 01:01:55 +0530

    okay

--- bot/cred.py
+++ bot/cred.py
@@ -1,4 +1,5 @@
 bot_token =  '5558568629:AAGHAg7y7Ilr778_WFtBx-Lik3R7p8sVsh8'
 bot_user_name = 'OneStopVideoDownloaderBot'
-URL = 'https://onestopyoutubedownloader.herokuapp.com/'
-#URL = 'https://9542-2405-201-4033-c04c-9435-61e2-85ec-222d.in.ngrok.io/'
+#URL = 'https://onestopyoutubedownloader.herokuapp.com/'
+#URL = 'https://ef3d-2405-201-4033-c85f-2825-2e28-5288-a78.in.ngrok.io/'
+URL = 'https://niharikarawatprojects.tech/bot2/'
--- bot/urls.py
+++ bot/urls.py
@@ -6,6 +6,6 @@ from bot.views import download, introduction
 urlpatterns = [
     path('5558568629:AAGHAg7y7Ilr778_WFtBx-Lik3R7p8sVsh8', introduction),
     path('set_webhook/', views.set_webhook),
-    path('download/<str:video_id>',download)
+    path('download/<str:video_id>', download)
 
 ]
